c5ebf90af750c1875d352d9573a9dd257681f4c8
FitzAudoen <smckon1@outlook.com>
2020-08-09 13:36:34 -0500

    set up with cookiecutter datascience

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Gas sensor analytics and calibration workflow.',
+    author='Spencer M.',
+    license='',
+)
