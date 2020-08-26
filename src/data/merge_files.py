32572f1882710d64b139cf0d82494fce0a60b25e
FitzAudoen <smckon1@outlook.com>
2020-08-26 18:52:50 -0500

    add sequential test number as pproxy for test date

--- src/data/merge_files.py
+++ src/data/merge_files.py
@@ -12,21 +12,29 @@ def merge_dat_files(out_fn):
     # Iterate over files and keep track of data
     ## Data did not appear to be in a standard format so needed custom processing
     data = []
-    for file_name in files:
-        with open(os.path.join(os.getcwd(),'data/raw',file_name)) as file:  # the a opens it in append mode
+    sample_no = 1
+    for batch in range(1,11):
+        with open(os.path.join(os.getcwd(),'data/raw','batch{}.dat'.format(batch))) as file:  # the a opens it in append mode
             for line in file:
                 line = line.strip()
                 line = line.split(' ')
-
+                #print(line)
+                #print('\n')
                 labels = [label for label in line[0].split(';')]
                 line_data = [float(datum.split(':')[1]) for datum in line[1:]]
                 line_data_feature_no = [str(int(datum.split(':')[0])) for datum in line[1:]]
 
+                #for datum in line[1:]: data[int(datum.split(':')[0])] = float(datum.split(':')[1])  
+                #print(line_data_labels)
+                #print(line_data)
+                #print('\n')
+                #print('\n')
                 if len(labels) !=2: print('not two labels')
                 if len(line_data_feature_no ) != 128: print('not 128 features')
-                #if line_data_labels != list(range(1,129)): print('lables misordered')
+                #if line_data_feature_no != list(range(1,129)): print('lables misordered')
 
-                data.append([file_name.split('.')[0]]+labels+line_data)
+                data.append([batch]+labels+[sample_no]+line_data)
+                sample_no+=1
 
     # Corresponses of chemical IDs from data source documentation
     chemical_names = {'1':'Ethanol', '2':'Ethylene', '3':'Ammonia', '4':'Acetaldehyde', '5':'Acetone', '6':'Toluene'}
@@ -36,7 +44,7 @@ def merge_dat_files(out_fn):
                     sensor_id, feature_id in itertools.product(range(1,9),range(1,17))]
 
     # Create dataframe
-    columns = ['Batch_ID','Gas_ID','Gas_Conc']  + feature_names
+    columns = ['Batch_ID','Gas_ID','Gas_Conc','sample_no'] + feature_names
     df = pd.DataFrame(data,columns=columns)
 
     for k, v in chemical_names.items():
